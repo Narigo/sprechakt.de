160118b341438286c0bc6ed60f16bdb61177089c
Narigo <Narigo@users.noreply.github.com>
2022-08-07 02:29:51 +0000

    Deploying to gh-pages from @ Narigo/sprechakt.de@5a861f28c75b4892a235fef6461156fbbfa7b13d 🚀

---
+++ _app/immutable/chunks/blog-9844eea3.js
@@ -0,0 +1,2 @@
+const e=[{id:"recanpxnonQDYsVvQ",authors:["recLC72TdO8ST8DBy"],body:`Der Sprechakt bekommt mit diesem Text seinen ersten Blogeintrag. Feiert mit uns, denn nur sofern diese Eintr\xE4ge gefeiert werden, werden sie auch weiterhin bef\xFCllt!pos
+`,shortDescription:"Ein kurzer Anrei\xDFertext st\xFCnde hier normalerweise.",status:"Public",title:"Auf ein Wort!"}];export{e as b};
