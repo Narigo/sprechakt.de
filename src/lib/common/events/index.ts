357a02641d08f921317cedae38e87f72c3db4570
Joern Bernhardt <joern.bernhardt@compose.us>
2022-08-09 01:15:46 +0200

    feat: Show only upcoming slams
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/common/events/index.ts
+++ src/lib/common/events/index.ts
@@ -1,5 +1,6 @@
 import head from '$lib/common/head';
 import type { SprechaktEvent } from '$lib/types';
+import dropWhile from '../dropWhile';
 
 export function getEventsById(events: SprechaktEvent[]): { [id: string]: typeof events[number] } {
 	return events.reduce((acc, event) => {
@@ -8,7 +9,15 @@ export function getEventsById(events: SprechaktEvent[]): { [id: string]: typeof 
 }
 
 export function getNextEvents(events: SprechaktEvent[]) {
-	return head(events, 5);
+	const startOfToday = new Date();
+	startOfToday.setHours(0);
+	startOfToday.setMinutes(0);
+	startOfToday.setSeconds(0);
+	startOfToday.setMilliseconds(0);
+	return head(
+		dropWhile(events, (event) => new Date(event.date) <= startOfToday),
+		5
+	);
 }
 
 export function getDate(event: SprechaktEvent): string {
