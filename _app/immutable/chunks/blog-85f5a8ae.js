72a88dbbbbe8d75397369206c39a9963554e065e
Narigo <Narigo@users.noreply.github.com>
2022-08-14 10:20:31 +0000

    Deploying to gh-pages from @ Narigo/sprechakt.de@8fbfcb06afee564d98148c2f548b4766d82bbad8 🚀

---
+++ _app/immutable/chunks/blog-85f5a8ae.js
@@ -0,0 +1,9 @@
+const e=[{id:"recanpxnonQDYsVvQ",authors:["recLC72TdO8ST8DBy"],body:`Der Sprechakt bekommt mit diesem Text seinen ersten Blogeintrag. Feiert mit uns, denn nur sofern diese Eintr\xE4ge gefeiert werden, werden sie auch weiterhin bef\xFCllt!
+
+Zun\xE4chst mag dieser Text den Anschein haben, dass es sich hierbei um reinen F\xFClltext handelt und mitnichten um ein \xE4u\xDFerst wertvolles Konstrukt einfachster Zeilen, die mit _kursiver_, **fett-markierter** oder gar ~~durchgestrichener~~ Schriftzeichen keinerlei Sinn und Zweck erf\xFCllt. Doch seid gewiss, geneigte Leserschaft, dass mit dieser durchaus wohlformatierten Aneinanderreihung eigent\xFCmlicher, manchmal sogar in \`fester Breite\` getippten W\xF6rter ein tats\xE4chliches Ziel verfolgt wird.
+
+Dem Ziel sind wir schon ein gutes St\xFCck n\xE4her gekommen.
+Ein einzelner Zeilenumbruch f\xFChrt uns nach einem letzten Paragraphen zu dem wahrlichen H\xF6hepunkt der gesamten \xDCbung:
+
+Hier ist er. Ein [Link zur Startseite](http:///).
+`,shortDescription:"Ein kurzer Anrei\xDFertext st\xFCnde hier normalerweise.",status:"Public",title:"Auf ein Wort!"}];export{e as b};
