d9dfe7f0190ce6745e0bcf252c27d4d486415f7a
Joern Bernhardt <joern.bernhardt@compose.us>
2022-08-15 12:39:55 +0200

    feat: Style image grid
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/posts/ImageGrid.svelte
+++ src/lib/posts/ImageGrid.svelte
@@ -4,6 +4,21 @@
 	export let images: readonly Attachment[];
 </script>
 
-{#each images as image}
-	<img src={image.url} alt="Eindruck vom Event" />
-{/each}
+<section>
+	{#each images as image}
+		<img src={image.url} alt="Eindruck vom Event" />
+	{/each}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 2em;
+	}
+
+	img {
+		box-shadow: 0 0 1em -2em #000;
+		max-width: 15em;
+	}
+</style>
