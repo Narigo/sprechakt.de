888ba2a60b25bc87fc2e457afdc98643d03ad69c
Joern Bernhardt <joern.bernhardt@compose.us>
2022-08-24 22:22:56 +0200

    feat: Add social links in footer
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,6 +1,12 @@
 <footer>
 	<slot />
-	<p>Weitere Informationen folgen bald...</p>
+	<div class="column">
+		<h4>Social links</h4>
+		<div class="socialIcons">
+			<a href="https://www.facebook.com/Sprechakt" rel="external">Facebook</a>
+			<a href="https://www.instagram.com/sprechakt_landshut/" rel="external">Instagram</a>
+		</div>
+	</div>
 </footer>
 
 <style>
@@ -14,5 +20,22 @@
 
 	footer > :last-child {
 		margin-bottom: 0;
+	}
+
+	.column {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+
+	.socialIcons {
+		display: flex;
+		gap: 1em;
+	}
+
+	h4 {
+		font-family: var(--font-heading);
+		margin: 0;
+		text-transform: uppercase;
 	}
 </style>
