c4591e0fb759a36cfbbea9857cb48f1b5242b4f3
Joern Bernhardt <joern.bernhardt@compose.us>
2022-09-14 22:16:35 +0200

    feat: Make ActCard more like a card
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/acts/ActCard.svelte
+++ src/lib/acts/ActCard.svelte
@@ -5,4 +5,34 @@
 	export let act: SprechAktAct;
 </script>
 
-<a href="{base}/infos/acts/{act.id}">{act.name}</a>
+<a href="{base}/infos/acts/{act.id}"
+	><div class="card">
+		{#if act.image}<div class="image"><img src={act.image.url} alt={act.name} /></div>{/if}
+		<div class="content">
+			<h3>{act.name}</h3>
+		</div>
+	</div></a
+>
+
+<style>
+	a {
+		overflow: hidden;
+		text-decoration: none;
+	}
+
+	.card {
+		display: flex;
+		flex-direction: column;
+		max-width: 15em;
+		overflow: hidden;
+	}
+
+	.image {
+		max-width: 100%;
+	}
+
+	img {
+		height: 100%;
+		width: 100%;
+	}
+</style>
