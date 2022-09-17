22c789a168a37f424abfcc2307cb12fea11f1186
Joern Bernhardt <joern.bernhardt@compose.us>
2022-09-17 21:53:14 +0200

    feat: Use image for Instagram link
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/acts/ActCard.svelte
+++ src/lib/acts/ActCard.svelte
@@ -25,7 +25,11 @@
 		{#if hasSocials}
 			<div class="socials">
 				{#if act.facebook}<a rel="external" href={act.facebook}>f</a>{/if}
-				{#if act.instagram}<a rel="external" href={act.instagram}>i</a>{/if}
+				{#if act.instagram}
+					<a rel="external" href={act.instagram}>
+						<img src={`${base}/socials/glyph-logo_May2016.svg`} alt="Instagram" />
+					</a>
+				{/if}
 				{#if act.twitter}<a rel="external" href={act.twitter}>t</a>{/if}
 				{#if act.youTube}<a rel="external" href={act.youTube}>yt</a>{/if}
 			</div>
@@ -61,6 +65,12 @@
 		display: flex;
 		gap: 1em;
 		justify-content: flex-end;
+	}
+
+	.socials a {
+		height: 2em;
+		padding: 0 0.5em;
+		width: 2.5em;
 	}
 
 	h3 {
