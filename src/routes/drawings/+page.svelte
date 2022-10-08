10a8dd50610cd098665ff29e37824096100cd295
Joern Bernhardt <joern.bernhardt@compose.us>
2022-10-08 19:22:33 +0200

    feat: Prepare page for slammaler
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/drawings/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import HeaderImage from '$lib/layout/HeaderImage.svelte';
+	import imageUrl from './slammaler.jpg';
+	import PageWithNavigation from '$lib/layout/PageWithNavigation.svelte';
+</script>
+
+<svelte:head>
+	<title>SprechAkt</title>
+	<meta name="description" content="Malereien" />
+</svelte:head>
+
+<PageWithNavigation>
+	<div slot="header">
+		<HeaderImage alt="Jörn, der Slammaler" position="center" {imageUrl} />
+	</div>
+
+	<section>Text folgt...</section>
+</PageWithNavigation>
