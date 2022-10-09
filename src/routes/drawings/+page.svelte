5e3a49f346b0096b78ad9e3217b2f20fad0a6080
Joern Bernhardt <joern.bernhardt@compose.us>
2022-10-09 16:23:42 +0200

    feat: Add some text to the page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/drawings/+page.svelte
+++ src/routes/drawings/+page.svelte
@@ -15,5 +15,22 @@
 		<HeaderImage alt="Jörn, der Slammaler" position="center" {imageUrl} />
 	</div>
 
-	<section>Text folgt...</section>
+	<section>
+		<p>
+			Eine Besonderheit des SprechAkt Slams ist, dass unsere Poet:innen am Ende kleine Kunstwerke zu
+			ihren vorgetragenen Texten geschenkt bekommen. Nahezu von Beginn an begleitet eine Stammjury
+			den SprechAkt. Ein fester Bestandteil dieses Bewertungsgremiums ist Jörn, der die
+			Bewertungstafeln mit Illustrationen dekoriert, die von dem jeweiligen Text inspiriert sind.
+			Diese verteilt er anschließend an die Poet:innen. Das führte im Lauf der Zeit dazu, dass die
+			nun viele unserer Jurys nicht nur Punkte vergeben, sondern sich auch künstlerisch betätigen.
+		</p>
+		<p>
+			Hier haben wir eine kleine Auswahl an Bildern zusammengestellt, die uns freundlicherweise
+			überlassen wurden - wer sein Bild wieder erkennt und als 'von unbekannt' deklariert wurde,
+			kann sich gerne bei uns melden! Weitere Bilder findet Ihr auch auf
+			<a href="https://www.instagram.com/slammaler/" rel="external"
+				>Jörns Instagramkanal (@slammaler)</a
+			>.
+		</p>
+	</section>
 </PageWithNavigation>
