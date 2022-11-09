f8ddc52d9eb2c45ae2559ec01ac49b5014375eac
Joern Bernhardt <joern.bernhardt@compose.us>
2022-11-09 11:11:16 +0100

    feat: Style image grid layout through flexbox
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/layout/PageWithNavigationAndGallery.svelte
+++ src/lib/layout/PageWithNavigationAndGallery.svelte
@@ -11,25 +11,34 @@
 <PageWithNavigation>
 	<slot slot="navigation" name="navigation" />
 	<slot slot="header" name="header" />
-	<slot />
-	<div />
-	<ImageGrid
-		images={images.flatMap((i) => {
-			return i.images.flatMap((image) => {
-				return {
-					alt: i.alt,
-					credits: i.credits,
-					title: i.title,
-					url: `${imageDb}/${image.id}/${image.filename}`
-				};
-			});
-		})}
-	/>
+	<div class="content-layout">
+		<div class="content">
+			<slot />
+		</div>
+		<ImageGrid
+			images={images.flatMap((i) => {
+				return i.images.flatMap((image) => {
+					return {
+						alt: i.alt,
+						credits: i.credits,
+						title: i.title,
+						url: `${imageDb}/${image.id}/${image.filename}`
+					};
+				});
+			})}
+		/>
+	</div>
 	<slot name="footer" />
 </PageWithNavigation>
 
 <style>
-	div {
-		height: 1em;
+	.content-layout {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+
+	.content {
+		flex: 1;
 	}
 </style>
