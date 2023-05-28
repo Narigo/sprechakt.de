b2d4d016c6d8006107c6465de356b7e133a5c94f
Joern Bernhardt <joern.bernhardt@compose.us>
2023-05-28 10:15:15 +0200

    fix: Sort acts by name
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/acts/+page.server.ts
+++ src/routes/acts/+page.server.ts
@@ -5,6 +5,15 @@ import type { PageServerLoad } from './$types';
 const acts = (dataActs as SprechAktAct[]).filter(
 	(act) => act.userTypes?.includes('poet') || act.userTypes?.includes('feature')
 );
+acts.sort((a, b) => {
+	if (a.name < b.name) {
+		return -1;
+	}
+	if (a.name === b.name) {
+		return 0;
+	}
+	return 1;
+});
 
 export const load: PageServerLoad = async () => {
 	return { acts };
