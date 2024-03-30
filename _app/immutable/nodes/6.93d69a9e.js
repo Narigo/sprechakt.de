58a5535c6eed591e04fdc54f8c27e48c83cc755c
Narigo <Narigo@users.noreply.github.com>
2024-03-30 18:35:49 +0000

    Deploying to gh-pages from @ Narigo/sprechakt.de@2ff2695f692d609411df9557a3ca2086c18372ef 🚀

---
+++ _app/immutable/nodes/6.93d69a9e.js
@@ -0,0 +1,41 @@
+import{S as ae,i as se,s as de,k as h,a as g,y as X,J as le,l as u,h as t,c as b,z as Y,n as Q,G as l,b as s,A as ee,g as te,d as ne,B as re,q as c,m as p,r as f,H as ie}from"../chunks/index.6e6d21e2.js";import{H as oe}from"../chunks/HeaderImage.f563a3da.js";import{P as me}from"../chunks/PageWithNavigation.12991065.js";const he=""+new URL("../assets/history.9f777739.svg",import.meta.url).href;function ue($){let n,o,r,d,a,m,S,G,O,H,w,V,E,z,x,L,v,R,J,A,y,K,_,F,I,k,C,M,B,D;return{c(){n=h("h2"),o=c("Die Anfänge"),r=g(),d=h("p"),a=c(`November 2013 - der erste SprechAkt Poetry Slam findet unter der Leitung von Sebastian Stopfer
+		und Sabine Oberpriller in der Alten Kaserne statt. Damals noch im Clubraum, doch wegen des
+		großen Besucherandrangs zog der Slam schon bei der dritten Veranstaltung in den großen Saal.`),m=h("br"),S=c(`
+		Seitdem wetteifern fünfmal pro Jahr Poet:Innen in der Alten Kaserne um den Slamgewinn. Hinzu kommen
+		Themenslams, beispielsweise im Rahmen der Aktion Klimazeit oder zum Weltfrauentag, bei Bücher Pustet
+		und der VHS Moosburg.`),G=h("br"),O=c(`
+		Einige Dichter begleiten den SprechAkt bereits nahezu von Beginn an, etwa Markus Berg, Albrecht Rau
+		und Bert Uschner, die heute noch regelmäßig in Landshut zu Gast sind.`),H=g(),w=h("h2"),V=c("Personelle Veränderungen"),E=g(),z=h("p"),x=c(`Im Organisationsteam gab es in den letzten Jahren mehrere Veränderungen: Sabine Oberpriller und
+		Sebastian Stopfer hielten nach der Gründung bis 2015 das Zepter in der Hand. Danach übergaben
+		sie den Slam an Mic Mehler und Christoph Hebenstreit vom Reimrausch Poetry Slam. Aufgrund von
+		Zeitnot reichten die beiden nach einem Jahr den Staffelstab an Sebastian Geiger und Kristina
+		Sterl (inzwischen Stopfer) weiter.`),L=g(),v=h("h2"),R=c("Einige Experimente"),J=g(),A=h("p"),y=c(`In der Zeit nach 2017 entstanden einige neue Formate - die bereits erwähnten Themenslams und
+		Leseabende, eine Open Stage in Joschi's Bar in der Landshuter Altstadt sowie der Grand Slam.
+		Letzterer leitete jeweils im Januar die Slam-Saison 2019 und 2020 ein. Nahezu 250 Besucher kamen
+		zu dem Wettstreit der Gewinner:Innen des Vorjahres, der zusätzlich durch besondere
+		Feature-Poeten wie Bas Böttcher im ersten und Alex Burkard im zweiten Jahr glänzte.`),K=g(),_=h("h2"),F=c("Instagram und die Corona-Jahre"),I=g(),k=h("p"),C=c(`Nach dem Ausscheiden von Kristina Sterl ergänzte Sandra Schörghuber 2019 das Organisationsteam.
+		Kurz darauf erblickte der Instagram-Kanal des SprechAkts das Licht der Welt und wurde zusammen
+		mit der bereits bestehenden Facebook-Seite Schauplatz einiger Online-Formate, die vor dem
+		Hintergrund der Pandemie entstanden: die 18-teilige Reihe Corona-Poetry, zwei weihnachtliche
+		Fortsetzungsgeschichten in der Adventszeit sowie den ersten Nachhaltigkeitsslam per Live-Stream
+		aus der VHS Moosburg.`),M=g(),B=h("h2"),D=c("2023 steht dem SprechAkt ein weiterer wichtiger Meilenstein bevor: Er wird 10!")},l(e){n=u(e,"H2",{});var i=p(n);o=f(i,"Die Anfänge"),i.forEach(t),r=b(e),d=u(e,"P",{});var P=p(d);a=f(P,`November 2013 - der erste SprechAkt Poetry Slam findet unter der Leitung von Sebastian Stopfer
+		und Sabine Oberpriller in der Alten Kaserne statt. Damals noch im Clubraum, doch wegen des
+		großen Besucherandrangs zog der Slam schon bei der dritten Veranstaltung in den großen Saal.`),m=u(P,"BR",{}),S=f(P,`
+		Seitdem wetteifern fünfmal pro Jahr Poet:Innen in der Alten Kaserne um den Slamgewinn. Hinzu kommen
+		Themenslams, beispielsweise im Rahmen der Aktion Klimazeit oder zum Weltfrauentag, bei Bücher Pustet
+		und der VHS Moosburg.`),G=u(P,"BR",{}),O=f(P,`
+		Einige Dichter begleiten den SprechAkt bereits nahezu von Beginn an, etwa Markus Berg, Albrecht Rau
+		und Bert Uschner, die heute noch regelmäßig in Landshut zu Gast sind.`),P.forEach(t),H=b(e),w=u(e,"H2",{});var N=p(w);V=f(N,"Personelle Veränderungen"),N.forEach(t),E=b(e),z=u(e,"P",{});var W=p(z);x=f(W,`Im Organisationsteam gab es in den letzten Jahren mehrere Veränderungen: Sabine Oberpriller und
+		Sebastian Stopfer hielten nach der Gründung bis 2015 das Zepter in der Hand. Danach übergaben
+		sie den Slam an Mic Mehler und Christoph Hebenstreit vom Reimrausch Poetry Slam. Aufgrund von
+		Zeitnot reichten die beiden nach einem Jahr den Staffelstab an Sebastian Geiger und Kristina
+		Sterl (inzwischen Stopfer) weiter.`),W.forEach(t),L=b(e),v=u(e,"H2",{});var Z=p(v);R=f(Z,"Einige Experimente"),Z.forEach(t),J=b(e),A=u(e,"P",{});var T=p(A);y=f(T,`In der Zeit nach 2017 entstanden einige neue Formate - die bereits erwähnten Themenslams und
+		Leseabende, eine Open Stage in Joschi's Bar in der Landshuter Altstadt sowie der Grand Slam.
+		Letzterer leitete jeweils im Januar die Slam-Saison 2019 und 2020 ein. Nahezu 250 Besucher kamen
+		zu dem Wettstreit der Gewinner:Innen des Vorjahres, der zusätzlich durch besondere
+		Feature-Poeten wie Bas Böttcher im ersten und Alex Burkard im zweiten Jahr glänzte.`),T.forEach(t),K=b(e),_=u(e,"H2",{});var j=p(_);F=f(j,"Instagram und die Corona-Jahre"),j.forEach(t),I=b(e),k=u(e,"P",{});var U=p(k);C=f(U,`Nach dem Ausscheiden von Kristina Sterl ergänzte Sandra Schörghuber 2019 das Organisationsteam.
+		Kurz darauf erblickte der Instagram-Kanal des SprechAkts das Licht der Welt und wurde zusammen
+		mit der bereits bestehenden Facebook-Seite Schauplatz einiger Online-Formate, die vor dem
+		Hintergrund der Pandemie entstanden: die 18-teilige Reihe Corona-Poetry, zwei weihnachtliche
+		Fortsetzungsgeschichten in der Adventszeit sowie den ersten Nachhaltigkeitsslam per Live-Stream
+		aus der VHS Moosburg.`),U.forEach(t),M=b(e),B=u(e,"H2",{});var q=p(B);D=f(q,"2023 steht dem SprechAkt ein weiterer wichtiger Meilenstein bevor: Er wird 10!"),q.forEach(t)},m(e,i){s(e,n,i),l(n,o),s(e,r,i),s(e,d,i),l(d,a),l(d,m),l(d,S),l(d,G),l(d,O),s(e,H,i),s(e,w,i),l(w,V),s(e,E,i),s(e,z,i),l(z,x),s(e,L,i),s(e,v,i),l(v,R),s(e,J,i),s(e,A,i),l(A,y),s(e,K,i),s(e,_,i),l(_,F),s(e,I,i),s(e,k,i),l(k,C),s(e,M,i),s(e,B,i),l(B,D)},p:ie,d(e){e&&t(n),e&&t(r),e&&t(d),e&&t(H),e&&t(w),e&&t(E),e&&t(z),e&&t(L),e&&t(v),e&&t(J),e&&t(A),e&&t(K),e&&t(_),e&&t(I),e&&t(k),e&&t(M),e&&t(B)}}}function ce($){let n,o;return n=new oe({props:{slot:"header",alt:"Historie",imageUrl:he}}),{c(){X(n.$$.fragment)},l(r){Y(n.$$.fragment,r)},m(r,d){ee(n,r,d),o=!0},p:ie,i(r){o||(te(n.$$.fragment,r),o=!0)},o(r){ne(n.$$.fragment,r),o=!1},d(r){re(n,r)}}}function fe($){let n,o,r,d;return r=new me({props:{$$slots:{header:[ce],default:[ue]},$$scope:{ctx:$}}}),{c(){n=h("meta"),o=g(),X(r.$$.fragment),this.h()},l(a){const m=le("svelte-1crxw28",document.head);n=u(m,"META",{name:!0,content:!0}),m.forEach(t),o=b(a),Y(r.$$.fragment,a),this.h()},h(){document.title="Historie",Q(n,"name","description"),Q(n,"content","Die Geschichte des SprechAkts Landshut und wie es zustande kam.")},m(a,m){l(document.head,n),s(a,o,m),ee(r,a,m),d=!0},p(a,[m]){const S={};m&1&&(S.$$scope={dirty:m,ctx:a}),r.$set(S)},i(a){d||(te(r.$$.fragment,a),d=!0)},o(a){ne(r.$$.fragment,a),d=!1},d(a){t(n),a&&t(o),re(r,a)}}}class Se extends ae{constructor(n){super(),se(this,n,null,fe,de,{})}}export{Se as component};
