acf9eac5e6b146a86d85bcf2c556b9219b7df308
Narigo <Narigo@users.noreply.github.com>
2026-01-05 18:45:59 +0000

    Deploying to gh-pages from @ Narigo/sprechakt.de@c43df09549cd2434f867d0e3354e90b19dc41d15 🚀

--- _app/immutable/chunks/stores.60eaf22f.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./singletons.85cef206.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
